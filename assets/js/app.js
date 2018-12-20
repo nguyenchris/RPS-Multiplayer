3fed8d11c2d7e3b023af69db202fbae81a5ecdc9
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2018-12-20 05:12:33 -0700

    added controllers as well as chat functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,62 @@
 // Initialize Firebase
+const config = {
+  apiKey: "AIzaSyA5ZKHNiSCU3b4iZJa-XvVhUT6Pz2HmVNY",
+  databaseURL: "https://myfirstproject-d800f.firebaseio.com",
+  projectId: "myfirstproject-d800f",
+  storageBucket: "myfirstproject-d800f.appspot.com",
+  messagingSenderId: "339468865019"
+};
 
-var dataController = (function () {
-  var config = {
-    apiKey: "AIzaSyA5ZKHNiSCU3b4iZJa-XvVhUT6Pz2HmVNY",
-    authDomain: "myfirstproject-d800f.firebaseapp.com",
-    databaseURL: "https://myfirstproject-d800f.firebaseio.com",
-    projectId: "myfirstproject-d800f",
-    storageBucket: "myfirstproject-d800f.appspot.com",
-    messagingSenderId: "339468865019"
-  };
+firebase.initializeApp(config);
 
-  firebase.initializeApp(config);
 
-  var database = firebase.database();
+
+const dataController = (function () {
+
+  // Firebase database reference
+  const dB = firebase.database();
+
+  // Object containing database references
+  const dbRef = {
+    chatRef: dB.ref('/chat'),
+    playersRef: dB.ref('players'),
+    // p1Ref: this.playersRef.child('p1'),
+    // p2Ref: this.playersRef.child('p2'),
+    turnRef: dB.ref('turn'),
+    connectedRef: dB.ref(".info/connected"),
+    connectionsRef: dB.ref("connections")
+  }
+
+
+  return {
+
+    // Return dbRef object
+    getDbRef: function () {
+      return dbRef;
+    }
+  }
+
 
 })();
 
 
 
 
-var chatController = (function () {
+
+const uiController = (function () {
+
+  const cacheDOM = {
+    $sendBtn: $('.send-btn'),
+    $chatText: $('#textarea2'),
+    $messages: $('#messages'),
+    $messageForm: $('#message-form')
+  }
+
+  return {
+    getCacheDOM: function () {
+      return cacheDOM;
+    }
+  }
 
 })();
 
@@ -29,40 +66,75 @@ var chatController = (function () {
 
 
 
-var gameController = (function () {
 
-})();
+const appController = (function (dataCtrl, uiCtrl) {
 
-
-
-
+  const dB = dataCtrl.getDbRef();
+  const dom = uiCtrl.getCacheDOM();
 
 
+  const setupEventListeners = () => {
+
+    dom.$sendBtn.on('click', addChatMsg);
+    dom.$messageForm.keypress((e) => {
+      // e.preventDefault();
+
+      if (e.which === 13) {
+        addChatMsg();
+      }
+    })
+
+  }
 
 
-var uiController = (function () {
 
-})();
+  const setupOnListeners = () => {
+
+    // test
+    dB.playersRef.set({
+      name: 'Chris'
+    })
+
+    dB.chatRef.on('child_added', (snap) => {
+      let html = `<p>${snap.val()}</p>` /// need to figure out how to add name of player and time during append
+      dom.$messages.append(html);
+      dom.$messages.scrollTop(dom.$messages[0].scrollHeight)
+    });
 
 
 
+    // dB.connectedRef.on("value", (snap) => {
+    //   if (snap.val() === true) {
+    //     alert("connected");
+    //   } else {
+    //     alert("not connected");
+    //   }
+    // });
+  }
 
 
 
+  const addChatMsg = () => {
 
+    let msg = dom.$chatText.val();
 
-var appController = (function (gmCtrl, uiCtrl, chCtrl) {
+    if (msg.length > 0) {
+      dB.chatRef.push(msg); /// need to figure out how to add name of player and time during push
+      dom.$chatText.val('');
+    }
+  }
 
 
 
   return {
 
     init: function () {
-      firebase.initializeApp(config);
+      setupOnListeners();
+      setupEventListeners();
     }
   }
 
-})(dataController, chatController, gameController, uiController);
+})(dataController, uiController);
 
-
+// initalize App
 appController.init();
